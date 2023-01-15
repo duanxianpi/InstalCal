b8b7139633812c935cc753c04c88d3348cf59e9a
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2023-01-14 20:16:08 -0500

    idk

--- instacal/src/components/upload/upload.jsx
+++ instacal/src/components/upload/upload.jsx
@@ -1,36 +1,69 @@
 import React from 'react';
 import "./upload.css";
 import { ChangeEvent, useRef, useState } from 'react';
+import {axios} from 'react';
+import { HiOutlineUpload } from 'react-icons/hi';
+import { Progress } from 'antd';
 
 
 export default function Upload() {
 
 
-    const [file, setFile] = useState();
-    const inputRef = useRef(null);
+  const [file, setFile] = useState();
+  const [fileURL, setFileURL] = useState();
+  const [uploaded, setUploaded] = useState(false);
+  const [progress, setProgress] = useState(0);
 
-    const handleUploadClick = () => {
-      // 👇 We redirect the click event onto the hidden input element
-      inputRef.current?.click();
-    };
 
-    const handleFileChange = (e) => {
-      if (!e.target.files) {
-        return;
-      }
+  const inputRef = useRef(null);
 
-      setFile(e.target.files[0]);
+  const handleUploadClick = () => {
+    // 👇 We redirect the click event onto the hidden input element
+    inputRef.current?.click();
+  };
 
-      // 🚩 do the file upload here normally...
-    };
+  const onUploadProgress = (progressEvent) => {
+    const { loaded, total } = progressEvent;
+    let percent = Math.floor((loaded * 100) / total);
+    if (percent < 100) {
+      setProgress(percent);
+    }
+  };
+
+  const uploadFile = (file) => {
+    const url = fileURL;
+    const formData = new FormData();
+    formData.append("file", file);
+    return axios.post(url, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      onUploadProgress,
+    });
+  };
+
+  const handleFileChange = (e) => {
+    if (!e.target.files) {
+      return;
+    }
+
+    setFile(e.target.files[0]);
+    setFileURL(URL.createObjectURL(e.target.files[0]));
+
+    setUploaded(true);
+    // 🚩 do the file upload here normally...
+  };
   return (
     <div className="Upload">
-      <div>Upload a file:</div>
-
-      {/* 👇 Our custom button to select and upload a file */}
-      <button onClick={handleUploadClick}>
-        {file ? `${file.name}` : 'Click to select'}
+      <button onClick={handleUploadClick} className="Button" style={{visibility:uploaded ? "hidden" : "visible"}}>
+      <HiOutlineUpload size={96} className="upload_icon" color="#2f2f2f"/>
+      <div style={{position:"absolute"}} className="btn_ele">
+      Upload an Image Here (png, jpg)
+      (Max Size: 20MB)
+      </div>
       </button>
+
+      <img src={fileURL} onClick={handleUploadClick} className="img" style={{display:uploaded ? "block" : "none"}}/>
 
       {/* 👇 Notice the `display: hidden` on the input */}
       <input
@@ -38,7 +71,11 @@ export default function Upload() {
         ref={inputRef}
         onChange={handleFileChange}
         style={{ display: 'none' }}
+        accept="image/png, image/jpeg"
       />
+
+      <Progress percent={progress} />
+
 
     </div>
 
