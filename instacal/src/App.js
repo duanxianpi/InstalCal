70d846ad627e87e5306976d7959fe21fdb3c66ec
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2023-01-14 23:20:51 -0500

    idk6

--- instacal/src/App.js
+++ instacal/src/App.js
@@ -4,11 +4,15 @@ import Title from "./components/title/title.jsx";
 
 import Upload from "./components/upload/upload.jsx";
 
+import Description from "./components/description/description.jsx";
+
+
 function App() {
   return (
     <div className="App">
       <Title />
       <Upload />
+      <Description />
     </div>
   );
 }
